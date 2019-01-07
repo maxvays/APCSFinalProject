a7226ebe078f5c21ffed265d79ac7f883d9c5869
Maxim Vaysburd <33438759+mwizard777@users.noreply.github.com>
2019-01-06 19:34:47 -0500

    Initialize Board class
    toString and main is for testing purposes.

--- Board.java
+++ Board.java
@@ -1,1 +1,36 @@
 
+public class Board {
+	private Checker[][] checkers;
+	
+	public static void main(String[] args){
+		Board board = new Board();
+		System.out.print(board);
+	}
+	
+	public Board(){
+		checkers = new Checker[8][8];
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+				if((i+j) % 2 == 1) checkers[i][j] = null;
+				else checkers[i][j] = new Checker(i, j, true);
+			}
+		}
+	}
+	
+	public String toString(){
+		String output = "  _ _ _ _ _ _ _ _\n";
+		String add;
+		for(int i = 0; i < 8; i++){
+			output = output + "|";
+			for(int j = 0; j < 8; j++){
+				if(checkers[i][j] == null){
+					add = "-";
+				}else{if(checkers[i][j].isRed()) add = "R"; else add = "B";}
+				output = output + " " + add;
+			}
+			output = output + " |\n";
+		}
+		output = output + "  _ _ _ _ _ _ _ _";
+		return output;
+	}
+}
