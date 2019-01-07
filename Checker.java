5ac2eb68fdc5b3bdace104e8bcb48fb6b2ea964e
Maxim Vaysburd <33438759+mwizard777@users.noreply.github.com>
2019-01-06 19:32:05 -0500

    Initialize class Checker with constructors and get methods

--- Checker.java
+++ Checker.java
@@ -1,1 +1,27 @@
 
+public class Checker {
+	private int row, column;
+	private boolean isRed;
+	
+	public Checker(int Row, int Column, boolean IsRed){
+		row  = Row;
+		column = Column;
+		isRed = IsRed;
+	}
+	
+	public int getRow(){
+		return row;
+	}
+	
+	public int getColumn(){
+		return column;
+	}
+	
+	public boolean isRed(){
+		return isRed;
+	}
+	
+	public String toString(){
+		return "Checker";
+	}
+}
